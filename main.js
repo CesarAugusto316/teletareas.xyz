0e7034a254aac011b64ef577b23674a1de701122
CesarAugusto316 <cesaraugusto316@protonmail.com>
2021-07-27 16:18:42 -0500

    circle slider implemented

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 
+// RANDOM QOUTE MACHINE GENERATOR //
 const Celebrities = {
     'quotes': [
         [['Un libro debe ser como un hacha para el mar helado que tenemos dentro de nosotros'], ['(Franz Kafka)']],
@@ -13,7 +14,7 @@ const Celebrities = {
         [['Una mentira puede llegar a viajar por medio mundo, mientras la verdad se está poniendo el calzado.'], ['(Mark Twain)']],
         [['Los monos son demasiado buenos para que el hombre pueda descender de ellos.'], ['(Friedrich Nietzsche)']],
         [['Si buscas la perfección, nunca estarás contento.'], ['(León Tolstói)']],
-        [['Los verdaderos amigos te apuñalan por delante.)'], ['(Oscar Wilde']],
+        [['Los verdaderos amigos te apuñalan por delante.'], ['(Oscar Wilde)']],
         [['En tiempos de engaños, decir la verdad es un acto revolucionario.'], ['(George Orwell)']],
         [['Como no me he preocupado de nacer, no me preocupo de morir.'], ['(Federico García Lorca)']],
         [['Un hombre ingenioso no se equivoca. Sus errores son volitivos y son portales del descubrimiento.'], ['(James Joyce)']],
@@ -40,3 +41,84 @@ const CELEBRITY = document.querySelector('#celebrity');
 
 QUOTE.textContent = `"${quote}"`;
 CELEBRITY.textContent = celebrity;
+// RANDOM QOUTE MACHINE GENERATOR //
+
+
+// SERVICES
+
+let i=2;
+
+	
+$(document).ready(function(){
+    var radius = 200;
+    var fields = $('.itemDot');
+    var container = $('.dotCircle');
+    var width = container.width();
+radius = width/2.5;
+
+     var height = container.height();
+    var angle = 0, step = (2*Math.PI) / fields.length;
+    fields.each(function() {
+        var x = Math.round(width/2 + radius * Math.cos(angle) - $(this).width()/2);
+        var y = Math.round(height/2 + radius * Math.sin(angle) - $(this).height()/2);
+        if(window.console) {
+            console.log($(this).text(), x, y);
+        }
+        
+        $(this).css({
+            left: x + 'px',
+            top: y + 'px'
+        });
+        angle += step;
+    });
+    
+    
+    $('.itemDot').click(function(){
+        
+        var dataTab= $(this).data("tab");
+        $('.itemDot').removeClass('active');
+        $(this).addClass('active');
+        $('.CirItem').removeClass('active');
+        $( '.CirItem'+ dataTab).addClass('active');
+        i=dataTab;
+        
+        $('.dotCircle').css({
+            "transform":"rotate("+(360-(i-1)*36)+"deg)",
+            "transition":"2s"
+        });
+        $('.itemDot').css({
+            "transform":"rotate("+((i-1)*36)+"deg)",
+            "transition":"1s"
+        });
+        
+        
+    });
+    
+    setInterval(function(){
+        var dataTab= $('.itemDot.active').data("tab");
+        if(dataTab>6||i>6){
+        dataTab=1;
+        i=1;
+        }
+        $('.itemDot').removeClass('active');
+        $('[data-tab="'+i+'"]').addClass('active');
+        $('.CirItem').removeClass('active');
+        $( '.CirItem'+i).addClass('active');
+        i++;
+        
+        
+        $('.dotCircle').css({
+            "transform":"rotate("+(360-(i-2)*36)+"deg)",
+            "transition":"2s"
+        });
+        $('.itemDot').css({
+            "transform":"rotate("+((i-2)*36)+"deg)",
+            "transition":"1s"
+        });
+        
+        }, 5000);
+    
+});
+
+
+// SERVICES
