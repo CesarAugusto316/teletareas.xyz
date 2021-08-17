b75201220d5482beb9c10c250e0d3cb814c619ea
cesaraugusto316 <cesaraugusto316@protonmail.com>
2021-08-16 23:48:07 -0500

    flask app: images linked

---
+++ app/views.py
@@ -0,0 +1,13 @@
+# from flask import render_template, request, url_for
+# from flask import Flask
+
+# app_01 = Flask(__name__)
+
+
+# @app_01.route('/')
+# def index():
+#     return render_template('index.html')
+
+
+# if __name__ == '__main__':
+#     app_01.run()
